ba9eb517b9420ec31b184522857fb8a2d119d5a5
Yan01h <yannickprivate60@gmail.com>
2024-04-30 15:15:20 +0200

    Removed PROJECT_NAME and PROJECT_ABBREVIATION vars from all CMakeLists.txt files

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 
 #include "cmake_config.h"
 
-auto main(int argc, char **argv) -> int
+auto main(int argc, char** argv) -> int
 {
     std::cout << "Hello World!\n";
     std::cout << PR_VERSION_STRING << "\n";
